f730e203bde314f3ba758a594ae86e517c2d93fe
cwckcy <checkzfx@gmail.com>
2025-01-29 23:49:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running at https://cwtkcy.onrender.com:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
